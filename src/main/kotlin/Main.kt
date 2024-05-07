e8e731b41dce3a678f6145b35422d32a37f4f725
Jordan Williams <jtw0213@gmail.com>
2024-05-07 17:06:11 -0500

    Update REPL to lex and parse

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,18 +2,38 @@ package org.example
 
 fun main() {
     val prompt = ">> "
+    val MONKEY_FACE = """
+            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-""${'"'}${'"'}${'"'}${'"'}${'"'}-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+    """.trimIndent()
 
     print(prompt)
-
     var input = readln()
     while (input != "exit") {
         val lexer = Lexer(input)
+        val parser = Parser(lexer)
 
-        var tok = lexer.nextToken()
-        while (tok.tokenType != TokenType.EOF) {
-            println("Token ${tok.tokenType} Literal ${tok.literal}")
-            tok = lexer.nextToken()
+        val program = parser.parseProgram()
+        if (parser.errors.isNotEmpty()) {
+            println(MONKEY_FACE)
+            println("Woops! We ran into some monkey business here!\n")
+            println("parser errors:\n")
+            for (e in parser.errors) {
+                println("\t $e \n")
+            }
+        } else {
+            println(program.toString())
         }
+
         println()
         print(prompt)
         input = readln()

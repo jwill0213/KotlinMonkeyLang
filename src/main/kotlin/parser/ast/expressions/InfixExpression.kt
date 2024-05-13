442d0c67c9a0a4fb6f69ffa1793fad8c4c70f992
Jordan Williams <jtw0213@gmail.com>
2024-05-13 17:11:26 -0500

    Eval infix expressions for ints

--- src/main/kotlin/parser/ast/expressions/InfixExpression.kt
+++ src/main/kotlin/parser/ast/expressions/InfixExpression.kt
@@ -1,6 +1,9 @@
 package org.example.parser.ast.expressions
 
 import org.example.lexer.Token
+import org.example.`object`.MonkeyInt
+import org.example.`object`.MonkeyNull
+import org.example.`object`.MonkeyObject
 
 class InfixExpression(private val token: Token, var left: Expression? = null) : Expression() {
     var operator: String = getTokenLiteral()
@@ -12,5 +15,26 @@ class InfixExpression(private val token: Token, var left: Expression? = null) : 
 
     override fun toString(): String {
         return "(${left.toString()} $operator ${right.toString()})"
+    }
+
+    override fun eval(): MonkeyObject {
+        val left = left?.eval() ?: MonkeyNull.NULL
+        val right = right?.eval() ?: MonkeyNull.NULL
+
+        if (left is MonkeyInt && right is MonkeyInt) {
+            return evalIntInfixExpression(left, right)
+        }
+
+        return MonkeyNull.NULL
+    }
+
+    private fun evalIntInfixExpression(left: MonkeyInt, right: MonkeyInt): MonkeyObject {
+        return when (operator) {
+            "+" -> MonkeyInt(left.value + right.value)
+            "-" -> MonkeyInt(left.value - right.value)
+            "*" -> MonkeyInt(left.value * right.value)
+            "/" -> MonkeyInt(left.value / right.value)
+            else -> MonkeyNull.NULL
+        }
     }
 }

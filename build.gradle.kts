630ecbfed83bac010f2cd7c5a04425da0549acc4
Jordan Williams <jtw0213@gmail.com>
2024-07-18 10:33:31 -0500

    Add uberJar to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    id("application")
 }
 
 group = "org.example"
@@ -16,6 +17,28 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
+}
+
+application {
+    mainClass = "org.example.InterpreterKt"
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "org.example.InterpreterKt"
+    }
+}
+
+tasks.register<Jar>("uberJar") {
+    archiveClassifier = "uber"
+
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
 }
